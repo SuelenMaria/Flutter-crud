10eaafde1b2f58ac1b0846e1bfa7e5f0149b1495
Suelen Maria <suzyhh271@gmail.com>
2023-12-03 21:10:22 -0300

    Insert, Change and Navigate

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
+import 'package:flutter_crud/views/user_form.dart';
 import 'package:flutter_crud/views/user_list.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Users(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        routes: {
+          AppRoutes.HOME: (_) => UserList(),
+          AppRoutes.USER_FORM: (_) => UserForm()
+        },
       ),
-      home: UserList(),
     );
   }
 }
--- lib/views/user_list.dart
+++ lib/views/user_list.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_crud/components/user.tile.dart';
-import 'package:flutter_crud/data/dummy_users.dart';
+import 'package:flutter_crud/models/user.dart';
+import 'package:flutter_crud/provider/users.dart';
+import 'package:flutter_crud/routes/app_routes.dart';
 
 class UserList extends StatelessWidget {
+  const UserList({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    final users = {...DUMMY_USERS};
+    var Provider;
+    final Users users = Provider.of(context);
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Lista de usuários'),
-          actions: <Widget>[
-            IconButton(onPressed: () {}, icon: Icon(Icons.add))
-          ],
-        ),
-        body: ListView.builder(
-          itemCount: users.length,
-          itemBuilder: (ctx, i) => UserTile(users.values.elementAt(i)),
-        ));
+      appBar: AppBar(
+        title: const Text('Lista de Usuários'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(AppRoutes.USER_FORM);
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: users.count,
+        itemBuilder: (ctx, i) => UserTile(users.byIndex(i)),
+      ),
+    );
   }
 }
